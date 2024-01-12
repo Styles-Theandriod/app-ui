a25ca393d0cd47790984b0ab7cfbf705e25b7e77
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-01-12 15:04:53 +0100

    a new file created

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Homescreen from './assets/component/Homescreen';
+import Quiz from './assets/component/Quiz';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Homescreen/>
+      {/* <Homescreen/> */}
       <Quiz/>
     </View>
   );
--- assets/component/Quiz.js
+++ assets/component/Quiz.js
@@ -1,17 +1,83 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Quiz = () => {
   return (
-    <View>
-      <Text>hhk</Text>
-      <Text>Quiz</Text>
-      <Text>lk</Text>
+    <View style={styles.wrap}>
+      <View style={styles.header}>
+      <Ionicons name="beaker-outline" size={30} color={"white"}></Ionicons>
+        <Text style={styles.title}>Science and Technologies</Text>
+        <Ionicons name="construct-outline" size={30} color={"white"}></Ionicons>
+      </View>
+
+      <View style={styles.quizApp}>
+        <Text style={styles.quiz1}>Quiz: 60</Text>
+        <Text style={styles.quiz1}>03:00mins</Text>
+      </View>
+
+    <View style={styles.container}> 
+
+    <Text style={styles.quiz2}>Quiz: 60</Text>
+    
+        <TouchableOpacity style={styles.quiz3}>
+        <TouchableOpacity style={styles.quiz3}>
+          <Ionicons name="aperture-outline" size={20} color={"white"}>
+          </Ionicons>
+            <Text>Art and Literature</Text>
+        </TouchableOpacity>
+        </TouchableOpacity>
+
     </View>
- );
+
+    </View>
+  );
 };
 
 export default Quiz;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrap: {
+    backgroundColor: "#1b3b50",
+    flex: 1,
+  },
+
+  header: {
+    backgroundColor: "red",
+    height: 120,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'space-around',
+    
+
+  },
+
+  quizApp:{
+    backgroundColor: "orange",
+    height:35,
+    marginTop:60,
+    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: 'space-around',
+    
+
+
+  },
+  title:{
+    color: "white",
+    fontSize: 20,
+    fontWeight: "900",
+  },
+
+  quiz1:{
+    color: "white",
+    fontSize: 20,
+    fontWeight: "900",
+},
+
+    quiz2:{
+        color: "white",
+        fontSize: 20,
+        fontWeight:900,
+    },
+});
